4c81c03aab60e77ae10750e6294478edb5301177
Dale Yarborough <daleyarborough@gmail.com>
2017-05-16 09:59:38 -0700

    Update geojson.js

--- geoJSON/geojson.js
+++ geoJSON/geojson.js
@@ -1,1 +1,13 @@
-**
+{
+  "type": "Feature",
+    "properties":{
+      "id":3,
+        "name": "Charlotte Park",
+          "image": "charlotte.jpg",
+            "web": "https://rooibus.com/image"
+    },
+      "geometry":{
+        "type": "Point",
+          "coordinates": [-99.18654, 19.41933]
+      }
+}
